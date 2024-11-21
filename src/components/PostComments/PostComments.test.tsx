31d3ebd7decffcda3cd456a0c047b584a0be0d7c
Gabriel Silva <gabrielssilva3107@gmail.com>
2024-11-21 19:33:47 -0300

    Adicionando teste de comentario

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,31 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar o primeiro comentário para verificar funcionalidade do teste', () => {
+        render(<PostComment />);
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: { value: 'Comentário teste' },
+    });
+
+    fireEvent.click(screen.getByTestId('comment-button'));
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: { value: 'Primeiro comentário' },
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+        target: { value: 'Segundo comentário' },
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
